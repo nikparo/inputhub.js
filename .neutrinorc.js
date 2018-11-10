5c91665bf8a0a32f0889f7401ae61daa2a0da812
Nik <niklas.paro@gmail.com>
2018-11-10 14:44:01 +0200

    Basic neutrino in place

---
+++ .neutrinorc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  use: [
+    // '@neutrinojs/airbnb-base',
+    [
+      '@neutrinojs/library',
+      {
+        name: 'inputhub',
+        target: 'web',
+        polyfills: {
+          async: true,
+        },
+        babel: {
+          presets: [
+            ['babel-preset-env', {
+              // Passing in browser targets to babel-preset-env will replace them
+              // instead of merging them when using the 'web' target
+              targets: {
+                browsers: [
+                  '> 0.2%',
+                  'last 1 version',
+                  'Firefox ESR',
+                  'not dead',
+                ],
+              },
+            }],
+          ],
+        },
+      },
+    ],
+    '@neutrinojs/jest',
+  ],
+};
